bfee41f2d1484fc6e5e0e7a65341a341ec002aa4
Troy Stribling <troy.stribling@gmail.com>
2017-07-22 09:59:08 -0700

    created project

---
+++ entropy/entropy/entropy.h
@@ -0,0 +1,19 @@
+//
+//  entropy.h
+//  entropy
+//
+//  Created by Troy Stribling on 7/22/17.
+//  Copyright © 2017 Troy Stribling. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for entropy.
+FOUNDATION_EXPORT double entropyVersionNumber;
+
+//! Project version string for entropy.
+FOUNDATION_EXPORT const unsigned char entropyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <entropy/PublicHeader.h>
+
+
---
+++ entropy/entropyTests/entropyTests.swift
@@ -0,0 +1,36 @@
+//
+//  entropyTests.swift
+//  entropyTests
+//
+//  Created by Troy Stribling on 7/22/17.
+//  Copyright © 2017 Troy Stribling. All rights reserved.
+//
+
+import XCTest
+@testable import entropy
+
+class entropyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
